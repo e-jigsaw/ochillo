e5049482a80095b00c06f5f9bc50cb384365ffc0
jigsaw <jigsaw@live.jp>
2012-09-06 16:31:34 +0900

    add ballController test tochuuuu・∀・

---
+++ test/ballControllerTest.js
@@ -0,0 +1,18 @@
+var assert = reqire("assert");
+
+describe("ボールコントローラは", function() {
+	var BallController = reqire("../src/ballController");
+
+	var ballController = new BallController();
+
+	before(function(done) {
+		done();
+	});
+
+	it("任意の玉のブロックを作ることができる");
+
+	it("玉のブロックをレーンを指定してを落とすことができる", function() {
+		ballController.createBalls();
+		
+	});
+});
