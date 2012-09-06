bde0b591004c4fc34a84159a8d0eaca4afbcadc9
jigsaw <jigsaw@live.jp>
2012-09-06 16:30:39 +0900

    add boardModel src and test

---
+++ src/boardModel.js
@@ -0,0 +1,78 @@
+var Black = 0;
+var White = 1;
+var Wall  = 9;
+var Empty = -1;
+
+function boardModel() {
+	var self = this;
+
+	this.state = [];
+
+	this.init = function() {
+		for(var i=0; i<=9; i++) {
+			for(var j=0; j<=9; j++) {
+				if(j == 0 || j == 9 || i ==9) self.state[i].push(Wall);
+				else self.state[i].push(Empty);
+			}
+		}
+	}
+
+	this.set = function(x, y, ball) {
+		self.state[x][y] = ball;
+	}
+
+	this.get = function(x, y) {
+		return self.state[x][y];
+	}
+
+	this.check = function(x, y, ball) {
+		self.searchBalls(x, y, ball, false, []);
+	}
+
+	this.searchBalls = function (x, y, ball, flg, queue) {
+		if(!flg) {
+			// 8方向に探索、同じもしくは空、壁だったら探索終了
+			if(self.get(x-1, y-1) == ball || self.get(x-1, y-1) == Empty || self.get(x-1, y-1) == Wall) return 0;
+			else arguments.callee(x-1, y-1, ball, true, queue.push([x, y]));
+			if(self.get(x, y-1) == ball || self.get(x, y-1) == Empty || self.get(x, y-1) == Wall) return 0;
+			else arguments.callee(x, y-1, ball, true, queue.push([x, y]));
+			if(self.get(x+1, y-1) == ball || self.get(x+1, y-1) == Empty || self.get(x+1, y-1) == Wall) return 0;
+			else arguments.callee(x+1, y-1, ball, true, queue.push([x, y]));
+			if(self.get(x-1, y) == ball || self.get(x-1, y) == Empty || self.get(x-1, y) == Wall) return 0;
+			else arguments.callee(x-1, y, ball, true, queue.push([x, y]));
+			if(self.get(x+1, y) == ball || self.get(x+1, y) == Empty || self.get(x+1, y) == Wall) return 0;
+			else arguments.callee(x+1, y, ball, true, queue.push([x, y]));
+			if(self.get(x-1, y+1) == ball || self.get(x-1, y+1) == Empty || self.get(x-1, y+1) == Wall) return 0;
+			else arguments.callee(x-1, y+1, ball, true, queue.push([x, y]));
+			if(self.get(x, y+1) == ball || self.get(x, y+1) == Empty || self.get(x, y+1) == Wall) return 0;
+			else arguments.callee(x, y+1, ball, true, queue.push([x, y]));
+			if(self.get(x+1, y+1) == ball || self.get(x+1, y+1) == Empty || self.get(x+1, y+1) == Wall) return 0;
+			else arguments.callee(x+1, y+1, ball, true, queue.push([x, y]));
+		} else {
+			// 8方向に探索、空、壁だったら探索終了
+			// 違う玉なら探索続行
+			if(self.get(x-1, y-1) == Empty || self.get(x-1, y-1) == Wall) return 0;
+			else if(self.get(x-1, y-1) == !ball) arguments.callee(x-1, y-1, ball, true, queue.push([x, y]));
+			if(self.get(x, y-1) == Empty || self.get(x, y-1) == Wall) return 0;
+			else if(self.get(x, y-1) == !ball) arguments.callee(x, y-1, ball, true, queue.push([x, y]));
+			if(self.get(x+1, y-1) == Empty || self.get(x+1, y-1) == Wall) return 0;
+			else if(self.get(x+1, y-1) == !ball) arguments.callee(x+1, y-1, ball, true, queue.push([x, y]));
+			if(self.get(x-1, y) == Empty || self.get(x-1, y) == Wall) return 0;
+			else if(self.get(x-1, y) == !ball) arguments.callee(x-1, y, ball, true, queue.push([x, y]));
+			if(self.get(x+1, y) == Empty || self.get(x+1, y) == Wall) return 0;
+			else if(self.get(x+1, y) == !ball) arguments.callee(x+1, y, ball, true, queue.push([x, y]));
+			if(self.get(x-1, y+1) == Empty || self.get(x-1, y+1) == Wall) return 0;
+			else if(self.get(x-1, y+1) == !ball) arguments.callee(x-1, y+1, ball, true, queue.push([x, y]));
+			if(self.get(x, y+1) == Empty || self.get(x, y+1) == Wall) return 0;
+			else if(self.get(x, y+1) == !ball) arguments.callee(x, y+1, ball, true, queue.push([x, y]));
+			if(self.get(x+1, y+1) == Empty || self.get(x+1, y+1) == Wall) return 0;
+			else if(self.get(x+1, y+1) == !ball) arguments.callee(x+1, y+1, ball, true, queue.push([x, y]));
+		}
+		// キューをひっくり返す処理
+		for(var i in queue) {
+			self.set(queue[i][0], queue[i][1], ball);
+		}
+	}
+}
+
+module.exports = boardModel;
---
+++ test/boardModelTest.js
@@ -0,0 +1,39 @@
+var assert = reqire("assert");
+
+describe("ボードモデルは", function() {
+	var BoardModel = reqire("../src/boardModel");
+	var Black = 0;
+	var White = 1;
+	var Wall  = 9;
+
+	var boardModel = new BoardModel();
+
+	before(function(done) {
+		boardModel.init();
+		done();
+	});
+
+	it("8x8のマップを持っている", function() {
+		assert.equal(boardModel.state.length, 10);
+		assert.equal(boardModel.state[0].length, 9);
+	});
+
+	it("座標を指定してボードの状態を変更できる", function() {
+		boardModel.set(2, 2, Black);
+		assert.equal(boardModel.state[2][2], Black);
+		boardModel.set(2, 2, White);
+		assert.equal(boardModel.state[2][2], White);
+	});
+
+	it("座標を指定してボードの状態を取得できる", function() {
+		boardModel.state[2][2] = Black;
+		assert.equal(boardModel.get(2, 2), Black);
+	});
+
+	it("盤面の判定ができる", function() {
+		boardModel.state[8][4] = Black;
+		boardModel.state[7][4] = White;
+		boardModel.check(6, 4, Black);
+		assert.equal(boardModel.state[7][4], Black);
+	});
+});
