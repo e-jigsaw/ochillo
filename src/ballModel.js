a4a890cd87a80fc23a9dd129946a3f10929e321a
jigsaw <jigsaw@live.jp>
2012-09-10 10:35:43 +0900

    fix empty value

--- src/ballModel.js
+++ src/ballModel.js
@@ -2,7 +2,12 @@ function ballModel() {
 	var self = this;
 
 	this.init = function() {
-		self.resetCurrentBoard();
+		for(var i=0; i<10; i++) {
+			self.currentBoard.push([]);
+			for(var j=0; j<10; j++) {
+				self.currentBoard[i].push(-1);
+			}
+		}
 	}
 
 	this.currentBalls = [];
@@ -15,50 +20,50 @@ function ballModel() {
 		switch(type) {
 			case 0:
 				self.nextBalls = [
-					[0, 1, 0, 0], 
-					[0, 1, 0, 0], 
-					[0, 1, 1, 0], 
-					[0, 0, 0, 0]
+					[-1, 1, -1, -1], 
+					[-1, 1, -1, -1], 
+					[-1, 1, 1, -1], 
+					[-1, -1, -1, -1]
 				];
 				break;
 			case 1:
 				self.nextBalls = [
-					[0, 0, 1, 0], 
-					[0, 0, 1, 0], 
-					[0, 1, 1, 0], 
-					[0, 0, 0, 0]
+					[-1, -1, 1, -1], 
+					[-1, -1, 1, -1], 
+					[-1, 1, 1, -1], 
+					[-1, -1, -1, -1]
 				];
 				break;
 			case 2:
 				self.nextBalls = [
-					[0, 1, 0, 0], 
-					[0, 1, 0, 0], 
-					[0, 1, 0, 0], 
-					[0, 1, 0, 0]
+					[-1, 1, -1, -1], 
+					[-1, 1, -1, -1], 
+					[-1, 1, -1, -1], 
+					[-1, 1, -1, -1]
 				];
 				break;
 			case 3:
 				self.nextBalls = [
-					[0, 0, 0, 0], 
-					[0, 1, 1, 0], 
-					[0, 1, 1, 0], 
-					[0, 0, 0, 0]
+					[-1, -1, -1, -1], 
+					[-1, 1, 1, -1], 
+					[-1, 1, 1, -1], 
+					[-1, -1, -1, -1]
 				];
 				break;
 			case 4:
 				self.nextBalls = [
-					[0, 0, 0, 0], 
-					[0, 1, 1, 0], 
-					[1, 1, 0, 0], 
-					[0, 0, 0, 0]
+					[-1, -1, -1, -1], 
+					[-1, 1, 1, -1], 
+					[1, 1, -1, -1], 
+					[-1, -1, -1, -1]
 				];
 				break;
 			case 5:
 				self.nextBalls = [
-					[0, 0, 0, 0],
-					[1, 1, 0, 0],
-					[0, 1, 1, 0],
-					[0, 0, 0, 0]
+					[-1, -1, -1, -1],
+					[1, 1, -1, -1],
+					[-1, 1, 1, -1],
+					[-1, -1, -1, -1]
 				];
 				break;
 		}
@@ -68,11 +73,14 @@ function ballModel() {
 		return self.currentBalls;
 	}
 
+	this.getCurrentBoard = function() {
+		return self.currentBoard;
+	}
+
 	this.resetCurrentBoard = function() {
 		for(var i=0; i<10; i++) {
-			self.currentBoard.push([]);
 			for(var j=0; j<10; j++) {
-				self.currentBoard[i].push(0);
+				self.currentBoard[i][j] = -1;
 			}
 		}
 	}
@@ -84,30 +92,42 @@ function ballModel() {
 	this.moveNext = function() {
 		self.currentBalls = self.nextBalls;
 		self.nextBalls = self.generateBalls(Math.floor(Math.random() * 5));
+		self.deployBalls(3, 0);
 	}
 
-	this.rollBalls = function() {
+	this.rollBalls = function(board) {
 		var tmpBalls = [];
+		var flg = 1;
+		
 		for(var i=0; i<self.currentBalls.length; i++) {
 			tmpBalls.push([]);
 			for(var j=0; j<self.currentBalls[i].length; j++) {
-				tmpBalls[i].push(0);
+				tmpBalls[i].push(-1);
 			}
 		}
+		
 		for(var i=0; i<self.currentBalls.length; i++) {	
 			for(var j=0; j<self.currentBalls[i].length; j++) {
 				tmpBalls[i][j] = self.currentBalls[j][i];
 			}
 		}
-		self.currentBalls = tmpBalls;
+		
+		for(var i=0; i<tmpBalls.length; i++) {
+			for(var j=0; j<tmpBalls[i].length; j++) {
+				if(board[j+self.currentY][i+self.currentX] != -1) flg = 0;
+			}
+		}
+
+		if(flg) self.currentBalls = tmpBalls;
 	}
 
 	this.deployBalls = function(x, y) {
 		self.currentX = x;
 		self.currentY = y;
-		for(var i=0; i<self.getCurrentBalls.length; i++) {
-			for(var j=0; j<self.getCurrentBalls[i].length; j++) {
-
+		self.resetCurrentBoard();
+		for(var i=0; i<self.getCurrentBalls().length; i++) {
+			for(var j=0; j<self.getCurrentBalls()[i].length; j++) {
+				self.getCurrentBoard()[j+y][i+x] = self.currentBalls[j][i];
 			}
 		}
 	}
